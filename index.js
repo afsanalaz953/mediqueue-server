995f559a6dea5eb54acd287f4c2882cbf3368279
afsanalaz953 <lazafsana7@gmail.com>
2026-05-27 18:22:21 +0600

    add-tutor-data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const dns = require('dns');
+const dns = require('node:dns');
 
-// Set custom DNS servers (Google DNS)
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// // Set custom DNS servers (Google DNS)
+ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const express = require('express')
 const app = express()
@@ -16,6 +16,8 @@ app.use (express.json());
 
 
 const uri = process.env.MONGO_URI;
+
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,16 +34,44 @@ await client.connect();
 
   const db = client.db("tutorData");
    const tutorCollections = db.collection("tutorCollection");
-
+   
+   const addingTutorCollections = db.collection("addingTutorCollection");
  const bookingCollections = db.collection("tutorBookingCollections");
 
-//   //  getting data from mongodatabase
- app.get('/tutors', async(req, res) => {
-const result= await tutorCollections.find().toArray()
+
+app.post ('/add-tutor', async(req, res)=>{
+ 
+    const formTutorData = req.body
+  console.log("form", formTutorData)
+     const result = await addingTutorCollections.insertOne(formTutorData)
+     res.json(result)
+ 
+   })
+
+
+
+//   //  getting data from mongodatabase for my-tutors page by clicking form
+ app.get('/my-tutors', async(req, res) => {
+const result= await addingTutorCollections.find().toArray()
 res.json(result);
-// console.log( "Alltutors", result)
+console.log( "Alltutors", result)
 
  })
+// // formTutorId 
+//    //   // for delete
+//  app.delete("/my-tutors/:formTutorId ", async(req, res) =>{
+// const {formTutorId } = req.params;
+// //  console.log("placeId", id);
+// // //  if get id then go to mongodoc for delete query
+// // // for particular id selection 
+// // const query = {_id : new ObjectId(id)}
+// const result = addingTutorCollections.deleteOne({_id:new ObjectId(formTutorId)});
+// // console.log(result);
+// res.json(result)
+
+//  });
+
+
 
  app.get('/featured', async(req, res) =>{
 const result = await tutorCollections.find().limit(6).toArray()
@@ -56,12 +86,8 @@ res.json(result)
 
 
 
-   // database creation
-app.post ('/tutors', async(req, res)=> {
-   const tutorData = req.body
-     const result = await tutorCollections.insertOne(tutorData)
-     res.json(result)
-   })
+   // 1) for formtutor data sending :database creation and send to mongo
+
 
 // app.patch ("/tutosr/:id", async(req,res)=>{
 // const {id}= req.params;
@@ -145,6 +171,15 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+app.get('/login',(req,res) =>{
+res.send("hello login page")
+
+})
+app.get('/register',(req,res) =>{
+res.send("hello register page")
+
+})
+
 
 app.listen(port, () => {
   console.log(`EXPREss server listening on port ${port}`)
